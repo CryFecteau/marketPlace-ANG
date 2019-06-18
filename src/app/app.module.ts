1b31a5cffec7b2d61b4babbdd15606956a003382
crystal fecteau and dylan crocker <crystal+dylan@>
2019-06-18 10:18:35 -0700

    style stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AboutComponent } from './about/about.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AlbumDetailComponent } from './album-detail/album-detail.component';
     WelcomeComponent,
     MarketplaceComponent,
     AboutComponent,
-    AlbumDetailComponent
+    AlbumDetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
